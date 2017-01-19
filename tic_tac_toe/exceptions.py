9911cd0a6647746bba70c1b19f3a256ef814fa6d
Josh Kopp <jkopp5549@gmail.com>
2017-01-19 06:08:56 +0000

    solution

--- tic_tac_toe/exceptions.py
+++ tic_tac_toe/exceptions.py
@@ -1,6 +1,10 @@
 class InvalidMovement(Exception):
+    """
+    Invalid Movement Exceptions:
+    msg1: The wrong player took a turn. Raises InvalidMovement: ___ moves next
+    msg2: The position is taken. Raises InvalidMovement: Position already taken.
+    msg3: The position is out of range. Raises InvalidMovement: Position out of range.
+    """
     pass
-
-
 class GameOver(Exception):
     pass
